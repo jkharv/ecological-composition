0776f66763a47fe53a0431e4ba4e1ddbf816ca77
Jake Harvey <jakekharvey@gmail.com>
2021-03-04 11:43:22 -0500

    Forgot some stuff in that last commit

--- sonic.rb
+++ sonic.rb
@@ -2,24 +2,26 @@ require 'csv'
 
 score = CSV.read("/Users/jake/Research Projects/ecological-composition/sll_score.csv", headers: true)
 
+score["sample"] = score["sample"].map{|num| num.to_i}
+score["delay"] = score["delay"].map{|num| num.to_r}
+score["speed"] = score["speed"].map{|num| num.to_r}
+
 block_length = 30
 
-puts score["sample"].max
-
-loop do # Between blocks
+score["sample"].max.times do |i| # Between blocks
   
-  loop do # Each block
+  score["sample"].count(i).times do |j| # Each block
     
     in_thread do
       
-      sleep (score[0]["delay"].to_r * block_length)
-      sample score[0]["instrument"], rate: score[0]["speed"]
-      
+      s = i + j
+      sleep (score[s]["delay"].to_r * block_length)
+      sample score[s]["instrument"], rate: score[s]["speed"]
+      puts "block " + i.to_s + " sample " + j.to_s
     end
-    
+    sleep 0.01
   end
   
   sleep block_length # Sleep until time for next block
-  
 end
 
